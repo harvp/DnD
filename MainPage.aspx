0ae59aab0b61e4fd2df307d5a2b9f97d7d04c907
harvp <69948085+harvp@users.noreply.github.com>
2020-11-04 21:56:35 -0500

    Add demo for MainPage

--- MainPage.aspx
+++ MainPage.aspx
@@ -8,8 +8,22 @@
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
-        </div>
-    </form>
+        
+<p>Choose a Class</p>
+    <asp:DropDownList id="ClassChoice" name="ClassChoice" runat="server">
+    <asp:ListItem selected="True" Value="none">Nothing</asp:ListItem>
+    <asp:ListItem value ="Barbarian">Barbarian</asp:ListItem>
+    <asp:ListItem value ="Bard">Bard</asp:ListItem>
+    <asp:ListItem value ="Rogue">Rogue</asp:ListItem>
+    <asp:ListItem value ="Ranger">Ranger</asp:ListItem>
+    </asp:DropDownList> 
+<p><asp:PlaceHolder ID="ControlContainer" runat="server" /></p>
+        <br />
+</form>
+<h3>Result:</h3>
+    <asp:PlaceHolder ID="Output" runat="server" />
+
+
+
 </body>
 </html>
--- MainPage.aspx.cs
+++ MainPage.aspx.cs
@@ -4,14 +4,39 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Diagnostics;
+using System.Web.Services.Description;
+using System.Configuration;
+using System.Text;
+using System.Xml.Linq;
 
 namespace DnD
 {
     public partial class MainPage : System.Web.UI.Page
     {
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            HtmlButton SubmitButton = new HtmlButton
+            {
+                ID = "Submitter",
+                InnerHtml = "Submit"
+            };
+            SubmitButton.Attributes.Add("style", "float: left;");
+            SubmitButton.ServerClick += new EventHandler(SubmitButton_Click);
+            ControlContainer.Controls.Add(SubmitButton);
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        void SubmitButton_Click(Object sender, EventArgs e)
+        {
+            string textFromForm = ClassChoice.Text;
+            Debug.WriteLine(textFromForm);
+            Output.Controls.Add(new LiteralControl("<textarea name=\"result2\" value= \"" + textFromForm + "\">" + textFromForm + "</textarea><br />"));
+            //< iframe name = "result2" style = "height:100px;width:200px;" ></ iframe >
         }
     }
 }
