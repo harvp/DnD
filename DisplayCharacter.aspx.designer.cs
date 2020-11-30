6a17442b4d1fd6f81388546045558988329715c3
Faith Sekerak <fms14@zips.uakron.edu>
2020-11-30 02:01:30 -0500

    Changes

--- DisplayCharacter.aspx.designer.cs
+++ DisplayCharacter.aspx.designer.cs
@@ -22,5 +22,14 @@ namespace DnD
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Select1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlSelect Select1;
     }
 }
