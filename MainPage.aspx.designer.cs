cf37df6f72dc2aa4ce1f9afa9c8fc08584109753
Faith Sekerak <fms14@zips.uakron.edu>
2020-11-29 23:21:48 -0500

    Main Page Changes

--- MainPage.aspx.designer.cs
+++ MainPage.aspx.designer.cs
@@ -22,32 +22,5 @@ namespace DnD
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
-        /// ClassChoice control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ClassChoice;
-
-        /// <summary>
-        /// ControlContainer control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.PlaceHolder ControlContainer;
-
-        /// <summary>
-        /// Output control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.PlaceHolder Output;
     }
 }
